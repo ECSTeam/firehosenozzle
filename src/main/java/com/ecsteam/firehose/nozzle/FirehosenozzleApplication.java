b2d230bdb7a5694b62aadd247af1fa4412c02b53
Dustin Ruehle <druehle@ecsteam.com>
2017-02-20 13:39:58 -0700

    initial commit

---
+++ src/main/java/com/ecsteam/firehose/nozzle/FirehosenozzleApplication.java
@@ -0,0 +1,12 @@
+package com.ecsteam.firehose.nozzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirehosenozzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirehosenozzleApplication.class, args);
+	}
+}
